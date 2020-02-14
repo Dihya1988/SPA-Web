7a28d161516dc5fad4e05fb21cee3033e40800f5
Nouha <balti.nouha@gmail.com>
2020-02-14 11:51:56 +0100

    Essai

---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { NavItem, NavLink } from 'reactstrap';
+import { Link, Route } from 'react-router-dom';
+
+
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+        <div>
+            
+
+            <h4 align="center">Welcome to Ref+, your intelligent DataHub!</h4>
+            
+                
+            
+      </div>
+    );
+  }
+}
